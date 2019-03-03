7e0c6c3fc722b64b1648b0c53ed544c19ac31714
sandhya naik <sandhyanaik@gmail.com>
2019-03-03 12:53:36 +0530

    added tan,cos

--- math.c
+++ math.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
+//changes did here
 void sin()
 {
-printf("print");
+printf("print sin");
+}
+void tan()
+{
+printf("print tan");
+}
+void cos()
+{
+printf("print cos");
+}
+void main()
+{
+sin();
+tan();
+cos();
 }
